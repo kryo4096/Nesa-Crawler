99003a01898a7d9d85ebcc4bc6dc10f56760d209
kryo4096 <kryo4096@gmail.com>
2016-12-07 20:34:38 +0100

    Functioning

---
+++ nesa_crawler.rb
@@ -0,0 +1,56 @@
+require 'mechanize'
+require 'io/console'
+
+
+if STDIN.respond_to?(:noecho)
+  def get_password()
+    print "Password: "
+    STDIN.noecho(&:gets).chomp
+  end
+else
+  puts "[Warning] Unsafe Password Entry"
+  def get_password()
+    print "Password (Will be visible, watch out!): "
+    gets.chomp
+  end
+end
+
+time = Time.now
+puts "Connecting..."
+bot = Mechanize.new{|a|a.verify_mode = OpenSSL::SSL::VERIFY_NONE}
+bot.get("https://kss.nesa-sg.ch/loginto.php?mode=0&lang=") do |page|
+  puts "Connected! (took #{Time.now-time} sec)"
+  print "Username: "
+  user = gets.chomp
+  password = get_password()
+  full_name = user.split(".").map(&:capitalize).join(" ")
+  puts
+  puts "Fetching Data..."
+
+  form = page.form("standardformular")
+  form.login = user
+  form.passwort = password
+  page = bot.submit(form)
+  page = bot.page.link_with(:text => 'Noten').click
+  grades = Hash.new
+  grades_rounded = Hash.new
+  page.search("table.list > tr").each do |tr|
+    items = tr.search("td").collect {|text| text.to_s}
+    unless items[0].nil?||items[1].nil? then
+      subject = items[0].to_s[/(\w{1,5}(?=-[1-4]))|f{2}\w*/]
+      grade = items[1].to_s.sub(/onmouseover=".*"/,"")[/([1-6]\.[0-9]{3})|---/]
+      grades[subject] = grade
+    end
+  end
+  grades.each do |subject,grade|
+    grades_rounded[subject.to_sym]=(2*grade.to_f).round.to_f / 2 unless grade=="---"
+  end
+
+
+
+
+
+  puts grades, grades_rounded
+
+
+end
